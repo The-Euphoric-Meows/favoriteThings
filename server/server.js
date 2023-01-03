8fcdf6b71c90cfe1e35d45003afad239ed3e7b5a
Patrick Reid <patrickjreid@gmail.com>
2023-01-02 20:05:05 -0700

    connected DB, created get and post requests.
    Co-authored-by: Etana Kopin <EGKopin@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
+const dotenv = require ('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 1963;
 const showRouter = require('./routes/showRouter');
+const connectDB = require('./schemas/db');
 
-app.use('/shows', )
+connectDB()
+
+app.use(express.urlencoded({extended:false}));
+
+app.use('/shows', showRouter);
 
 app.get('/', (req, res) => {
   res.send('hello nurse')
 })
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occured' }
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(PORT, () => console.log(`Listening on the port of our TimeLord, ${PORT}`))
